1d9a7db7a5e6ddfb685ea58a6cc78cf3fd8e0047
brandon roberts <8brandon@protonmail.com>
2020-03-21 19:08:10 +0100

    Fetch makes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "./components/Layout/Layout";
 
 function App() {
+  const [makes, setMakes] = useState([]);
+
+  useEffect(() => {
+    fetchMakes();
+  }, []);
+
+  const fetchMakes = async () => {
+    try {
+      const res = await fetch("http://localhost:8080/api/makes");
+      const data = await res.json();
+      setMakes(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return <Layout></Layout>;
 }
 

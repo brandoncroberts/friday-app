0b77d9335341a4c0491684b041cfd8b77fafbe7f
brandon roberts <8brandon@protonmail.com>
2020-03-22 14:54:15 +0100

    Limit number of items based on state value

--- src/components/VehicleList/VehicleList.js
+++ src/components/VehicleList/VehicleList.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import VehicleCard from "../VehicleCard/VehicleCard";
 
 const VehicleList = ({ vehicles, loading }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [vehiclesPerPage, setVehiclesPerPage] = useState(20);
+
   if (loading) {
     return <div>Loading.....</div>;
   }
+
+  const lastVehicle = currentPage * vehiclesPerPage;
+  const firstVehicle = lastVehicle - vehiclesPerPage;
+  const currentVehicles = vehicles.slice(firstVehicle, lastVehicle);
 
   return (
     <div>
       {vehicles.length ? (
         <div>
           Vehicles:
-          {vehicles.map((vehicle, index) => (
+          {currentVehicles.map((vehicle, index) => (
             <VehicleCard data={vehicle} key={index} />
           ))}
         </div>

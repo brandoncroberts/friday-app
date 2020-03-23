1d97bc476f6bacc1ffb2a6400468aab091e0fdad
brandon roberts <8brandon@protonmail.com>
2020-03-23 07:57:16 +0100

    Add styles to menu

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,6 +5,7 @@ import mainStyles from "./Main.module.css";
 const Main = ({ makes, history }) => {
   return (
     <div className={mainStyles.container}>
+      <h2>Search By Make & Model</h2>
       <MakesMenu makes={makes} history={history} />
     </div>
   );
--- src/components/MakesMenu/MakesMenu.js
+++ src/components/MakesMenu/MakesMenu.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import makesMenuStyles from "./MakesMenu.module.css";
 const MakesMenu = ({ makes, history }) => {
   const [value, setValue] = useState("");
   const [model, setModel] = useState("");
@@ -35,36 +35,35 @@ const MakesMenu = ({ makes, history }) => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Choose a vehicle make
-          <select onChange={handleChange} value={value}>
-            <option value="Choose a make of car">Choose a make of car</option>
-            {makes.map((make, index) => (
-              <option key={index} value={make}>
-                {make}
-              </option>
-            ))}
-          </select>
-        </label>
+    <div className={makesMenuStyles.container}>
+      <form className={makesMenuStyles.form} onSubmit={handleSubmit}>
+        <select onChange={handleChange} value={value}>
+          <option value="Choose a make of car">Choose a make of car</option>
+          {makes.map((make, index) => (
+            <option key={index} value={make}>
+              {make}
+            </option>
+          ))}
+        </select>
 
-        <label>
-          Choose a vehicle model
-          <select
-            onChange={handleModelChange}
-            value={model}
-            disabled={value ? false : true}
-          >
-            <option value={"Select a model"}>{"Select a model"}</option>
-            {models.map(model => (
-              <option key={model} value={model}>
-                {model}
-              </option>
-            ))}
-          </select>
-        </label>
-        <input type="submit" value="Search" disabled={value ? false : true} />
+        <select
+          onChange={handleModelChange}
+          value={model}
+          disabled={value ? false : true}
+        >
+          <option value={"Select a model"}>{"Select a model"}</option>
+          {models.map(model => (
+            <option key={model} value={model}>
+              {model}
+            </option>
+          ))}
+        </select>
+        <input
+          className={makesMenuStyles.submit}
+          type="submit"
+          value="Search"
+          disabled={value ? false : true}
+        />
       </form>
     </div>
   );

c6169bf9e3c8fdd3e5f27eb5b740d02db4758e2d
brandon roberts <8brandon@protonmail.com>
2020-03-23 17:35:03 +0100

    Remove unused prop

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,7 +2,7 @@ import React from "react";
 import MakesMenu from "../MakesMenu/MakesMenu";
 import mainStyles from "./Main.module.css";
 
-const Main = ({ makes, history, location, match }) => {
+const Main = ({ history, location, match }) => {
   return (
     <div className={mainStyles.container}>
       <h2>Search By Make & Model</h2>
